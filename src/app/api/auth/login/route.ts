28739939f6a166a6920b074a39ed2d2dd09ef08d
Kundan Pradhan <kundanpradhan1402@gmail.com>
2025-04-07 22:36:17 +0530

    feat: Added Toast functionality and optimized error handeling

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,9 +1,11 @@
-import { prisma } from "@/lib/prisma"; 
+import { prisma } from "@/lib/prisma";
 import bcrypt from "bcryptjs";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
   const { userName, userPassword } = await req.json();
+  const SUCCESS_MSG: string = "Logged in Successfully";
+  const ERROR_MSG: string = "Invalid Credentials";
 
   if (!userName || !userPassword) {
     return NextResponse.json(
@@ -17,20 +19,17 @@ export async function POST(req: NextRequest) {
   });
 
   if (!userCreds) {
-    return NextResponse.json(
-      { message: "Invalid Credentials" },
-      { status: 401 }
-    );
+    return NextResponse.json({ message: ERROR_MSG }, { status: 401 });
   }
 
   const isValid = await bcrypt.compare(userPassword, userCreds.password_hash);
 
   if (isValid) {
     return NextResponse.json(
-      { message: "Logged in as Admin", lastLogin: userCreds.last_login },
+      { message: SUCCESS_MSG, lastLogin: userCreds.last_login },
       { status: 200 }
     );
+  } else {
+    return NextResponse.json({ message: ERROR_MSG }, { status: 401 });
   }
-
-  return NextResponse.json({ message: "Invalid Credentials" }, { status: 401 });
 }
